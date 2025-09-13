1db940b9a5acda41078c6ac56c6c8c1447e0333c
Michael McCormack <mcctwin@gmail.com>
2025-09-12 21:42:12 -0500

    Create test suite and add solution

---
+++ lib/testing/test_palindrome.py
@@ -0,0 +1,13 @@
+import pytest
+
+from lib.palindrome import longest_palindromic_substring
+
+def test_is_palindrome():
+    assert longest_palindromic_substring("racecar") == "racecar"
+    assert longest_palindromic_substring("") == ""
+    assert longest_palindromic_substring("a") == "a"
+    assert longest_palindromic_substring("ab") == "a"
+    assert longest_palindromic_substring("aba") == "aba"
+    assert longest_palindromic_substring("abc") == "a"
+    assert longest_palindromic_substring("babad") in ["bab", "aba"]
+    assert longest_palindromic_substring("cbbd") == "bb"
